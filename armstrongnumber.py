6f9f2076f0e80a5ecce2052e18ccc0f7c19e6c62
mgokayb <47184520+mgokayb@users.noreply.github.com>
2019-10-19 13:26:05 +0300

    Update armstrongnumber.py

--- armstrongnumber.py
+++ armstrongnumber.py
@@ -1,9 +1,21 @@
+
 number = input("Give me a number for checking its an armstrong number or not >>>> ")
-result = 0
-for i in range(len(number)):
-    a = int(number[i])
-    result += a * a * a * a
-if result == int(number):
-    print(f"This number: ({number}) is an armstrong number.")
-else:
-    print(f"This number: ({number}) is NOT an armstrong number.")
+while True:
+    try:
+       val = int(number)
+       result = 0
+       for i in range(len(number)):
+           a = int(number[i])
+           result += a * a * a * a
+       if result == int(number):
+           print(f"This number: ({number}) is an armstrong number.")
+           number = input("Give me a number for checking its an armstrong number or not >>>> ")
+       else:
+           print(f"This number: ({number}) is NOT an armstrong number.")
+           number = input("Give me a number for checking its an armstrong number or not >>>> ")
+    except ValueError:
+        if number == "quit":
+            break
+        else:
+            print("That's not a number!")
+            number = input("Give me a number for checking its an armstrong number or not >>>> ")
