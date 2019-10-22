7409ba802259cc068b9bd7ce3d081e5709452a01
mgokayb <47184520+mgokayb@users.noreply.github.com>
2019-10-22 14:49:15 +0300

    Update a-kind-of-translator.py

--- a-kind-of-translator.py
+++ a-kind-of-translator.py
@@ -3,11 +3,31 @@ while True:
 
         translation = ""
         for letter in phrase:
-            if letter.lower() in "aeıioöuü":
+            if letter.lower() in "a":
                 if letter.isupper():
-                    translation = translation + "G"
+                    translation = translation + "4"
                 else:
-                    translation = translation + "g"
+                    translation = translation + "4"
+            elif letter.lower() in "e":
+                if letter.isupper():
+                    translation = translation + "3"
+                else:
+                    translation = translation + "3"
+            elif letter.lower() in "ıi":
+                if letter.isupper():
+                    translation = translation + "1"
+                else:
+                    translation = translation + "1"
+            elif letter.lower() in "oö":
+                if letter.isupper():
+                    translation = translation + "0"
+                else:
+                    translation = translation + "0"
+            elif letter.lower() in "uü":
+                if letter.isupper():
+                    translation = translation + "W"
+                else:
+                    translation = translation + "w"
             else:
                 translation = translation + letter
         return translation
@@ -18,4 +38,3 @@ while True:
         break
     else:
         print(translate(given_phrase))
-
